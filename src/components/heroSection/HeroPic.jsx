55ceb71186119521de8550cd5491d1fc0d408a90
Yashu257 <yashwanthkothapalli454@gmail.com>
2025-04-14 01:52:53 +0530

    Update site

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,3 +1,4 @@
+import pic from "../../assets/pic.jpg";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 import { PiHexagonThin } from "react-icons/pi";
@@ -11,7 +12,7 @@ const HeroPic = () => {
       viewport={{ once: false, amount: 0 }}
       className=" h-full flex items-center justify-center "
     >
-      <img src="../../src/assets/pic.jpg" alt="Yashwanth K" className="max-h-[450px] w-auto"/>
+      <img src={pic} alt="Yashwanth K" className="max-h-[450px] w-auto"/>
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
       </div>
