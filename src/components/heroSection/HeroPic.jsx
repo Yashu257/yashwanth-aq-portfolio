d2a816af93fafa09d624b274e6b137359ffbd3b7
Yashu257 <yashwanthkothapalli454@gmail.com>
2025-04-14 16:23:37 +0530

    Update site

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img src="profile.jpg"
-      alt="Yashwanth K" className="max-h-[450px] w-auto"/>
+      alt="Yashwanth K" className="hexagon-mask w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"/>
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
       </div>
