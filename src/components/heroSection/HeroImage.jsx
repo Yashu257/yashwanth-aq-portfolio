5d89879abd334c2ca38b1c85d0e790e69572beeb
Yashu257 <yashwanthkothapalli454@gmail.com>
2025-04-15 01:30:32 +0530

    Update site

--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -4,7 +4,7 @@ const HeroImage = () => {
   return (
     <div className="relative self-end h-full w-full items-center justify-center">
       <div className="h-full w-full">
-      <img src="mine.png"
+      <img src="naa.png"
 
           alt="Yashwanth K"
           className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
