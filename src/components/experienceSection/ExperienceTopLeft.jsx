f5fa2a1cba30301faefa3d28a0adeb9368dd33a4
Yashu257 <yashwanthkothapalli454@gmail.com>
2025-04-14 15:33:47 +0530

    Update site

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -4,12 +4,12 @@ const ExperienceTopLeft = () => {
   return (
     <div className="flex flex-col gap-6 w-[300px]">
       <p className="text-orange font-bold uppercase text-3xl font-special text-center">
-        Since 2022
+        Since 2024
       </p>
       <div className="flex justify-center items-center gap-4">
         <ExperienceInfo number="6" text="Months" />
         <p className="font-bold text-6xl text-lightBrown">-</p>
-        <ExperienceInfo number="23" text="Websites" />
+        <ExperienceInfo number="10" text="Client Projects" />
       </div>
       <p className="text-center">
       With 6 months of experience working as an AI Engineer/Data Scientist intern, I specialize in building intelligent, data-driven systems. During my internship at Rubix Company, I have been involved in implementing scalable solutions, enhancing the performance of web applications, and developing reusable components. I am passionate about solving real-world problems through AI and machine learning techniques while prioritizing user experience
